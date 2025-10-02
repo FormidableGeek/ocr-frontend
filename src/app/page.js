f334e9b4e222ada38764ce57d9fc7af449ccd429
Emeka Okonkwo <kaluokonkwoemeka@gmail.com>
2025-10-02 10:30:08 +0100

    Updated pages

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ const Home = () => {
                 <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
                     <div className="flex justify-center pt-8 sm:justify-start sm:pt-0">
 
-                        <ApplicationLogo></ApplicationLogo>
+                        <ApplicationLogo />
 
                     </div>
 
